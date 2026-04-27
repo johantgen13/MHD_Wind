c0e720816b772bef19656126e747d981a35d7615
johantgen13 <bjohantgen13@outlook.com>
2026-04-27 18:17:47 -0400

    Apr 27 Commit

---
+++ magneto/src/lib.rs
@@ -0,0 +1,4 @@
+// This is the rust file containing the logic for the simulation.
+// 
+// Author: Brayden JoHantgen
+// Last Update: 4/27/2026
--- magneto/src/main.rs
+++ magneto/src/main.rs
@@ -1,25 +1,44 @@
 // This is the rust file containing the 1D relativistic mhd simulation.
 // 
 // Author: Brayden JoHantgen
-// Last Update: 4/23/2026
+// Last Update: 4/27/2026
 
+use std::fs;
+use std::env;
+
+/////////////////
+/// Dataclasses
+/////////////////
 struct Config {
-
+    num_zones: u8,
+    dr: f64,
 }
 
-struct Physics {
-    let rho: f64;
-    let v_r: f64;
-    let E_density: f64;
-}
+//struct Physics {
+//}
 
-struct Driver {
+//struct Driver {
+//}
 
-}
+///////////////
+/// Functions
+///////////////
+//fn read_config_file(args: &[String]) -> Config {
+//    let num_zones = args[1].clone();
+//    let dr = 1.0 / (num_zones as f64);
+//
+//    Config { num_zones , dr}
+//}
 
-/////////////////
-// Simulation
-/////////////////
+//fn cartesian_to_polar(x:f64, y:f64) {
+//    r = sqrt((x*x) + (y*y))
+    //theta = np.arctan(y / x)
+    //return r, theta
+//}
+
+////////////////
+/// Simulation
+////////////////
 fn main() {
-    println!("Hello, world!");
+
 }
